901e76763622883d34560769834ecba8c729fa1e
Jakub Kontra <kontrajakub2@gmail.com>
2025-06-15 20:09:52 +0000

    progressing the markup

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Hooks,COntext API, Tailwind, Typescript, Autentifikacia
 
+1. 55 minuta
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,19 @@
 export default function Header() {
-  return <header className="col-[1/3] row-[1/2] bg-stone-100"></header>;
+  return (
+    <header
+      className="flex justify-between items-center
+                       col-[1/3] row-[1/2] bg-stone-100
+                       px-6"
+    >
+      <div className="flex gap-1">
+        <div className="bg-slate-400 w-[16px] h-[16px] rounded-full"></div>
+        <div className="bg-orange-400 w-[16px] h-[16px] rounded-full"></div>
+        <div className="bg-green-400 w-[16px] h-[16px] rounded-full"></div>
+      </div>
+
+      <div>
+        <b>0</b> / 0 todos completed
+      </div>
+    </header>
+  );
 }
